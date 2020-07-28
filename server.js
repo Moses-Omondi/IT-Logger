2ecd500cf885d8a3d2b06cf1c4ecb5445cfdf70c
Moses-Omondi <mose.omondi@gmail.com>
2020-07-28 18:01:43 +0530

    server.js update

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("it-logger/build"));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "it-logger", "build", "index.html"))
   );
 }
