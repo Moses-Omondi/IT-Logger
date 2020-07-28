d298a71e46839a99a083ee6745c4408bff428282
Moses-Omondi <mose.omondi@gmail.com>
2020-07-28 17:04:58 +0530

    Prepare for deploy

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
